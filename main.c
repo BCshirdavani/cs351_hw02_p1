fc7f867fdb94d423e7b6415470ec3b6c3896fa2c
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-02-14 10:11:40 -0800

    simple test
    testing c compilation with a simple "hello world"

---
+++ main.c
@@ -0,0 +1,17 @@
+//*********************************************
+//
+//	Author:			Beau Shirdavani
+//	Date:			2-14-2018
+//	Descritption:	CS 351 - HW 02
+//					read contacts list from .txt
+//					convert to contacts data type in C
+//
+//*********************************************
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
