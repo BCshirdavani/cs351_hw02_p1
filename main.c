51c80459e09cb055f1d82ec24da78fa033e2bdfb
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-02-15 10:05:14 -0800

    part 1 - complete
    part 1 is done.

--- main.c
+++ main.c
@@ -33,8 +33,6 @@ struct contact
 
 int main()
 {
-   printf("\tHello, World!\n");
-
    // read the text file
    FILE *file;
    char f_name[15];
@@ -53,7 +51,7 @@ int main()
    int i = 0;
    while (fscanf(file, "%s %s %d-%d-%d", &f_name, &l_name, &day, &mo, &yr) >1){
    	
-   	printf("%s %s %d %d %d\n", f_name, l_name, day, mo, yr);
+   	// printf("%s %s %d %d %d\n", f_name, l_name, day, mo, yr);
 
    	// put the text file data into the array of structures
    	c_list[i].c_date.d_day = day;
@@ -65,14 +63,13 @@ int main()
    		c_list[i].lName[j] = l_name[j];
    	}
 
-   	printf("\t");
-   	printf(c_list[i].fName);
-   	printf("\t");
-   	printf("\n\tyear: %d",c_list[i].c_date.d_yr);
-   	printf("\n");
-
-   	printf("loop: %d" , i);
-   	printf("\n");
+   	// printf("\t");
+   	// printf(c_list[i].fName);
+   	// printf("\t");
+   	// printf("\n\tyear: %d",c_list[i].c_date.d_yr);
+   	// printf("\n");
+   	// printf("loop: %d" , i);
+   	// printf("\n");
    	// increment the counter, for the next array element
    	i = i + 1;
    }
@@ -83,18 +80,18 @@ int main()
 
 	fclose(file);
 
-	printf("Date is: ");
-  	printf(__DATE__);
-  	printf("\n");
+	// printf("Date is: ");
+ //  	printf(__DATE__);
+ //  	printf("\n");
 
   	// filter and print ages
   	int k = 0;
   	for (k = 0; k < 15; k++){
   		if ( ((c_list[k].c_date.d_yr) != NULL) && ((2018 - (c_list[k].c_date.d_yr) ) > 17) && ((2018 - (c_list[k].c_date.d_yr) ) < 25)){
   			printf("\t");
-  			printf("name: %s", c_list[k].fName);
-  			printf("\t\t");
-  			printf("age: %d", (2018 - (c_list[k].c_date.d_yr)));
+  			printf("\t %c. %s, %d", c_list[k].fName[0], c_list[k].lName, (2018 - (c_list[k].c_date.d_yr)));
+  			// printf("\t\t");
+  			// printf("age: %d", (2018 - (c_list[k].c_date.d_yr)));
   			printf("\n");
   		}
   	}
